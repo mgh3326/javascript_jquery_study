5ae34a6cad880b8f94f6fdc3444b6778874c6a6c
mgh3326 <mgh3326@naver.com>
2018-04-25 13:37:00 +0900

    update

---
+++ 01부_자바스크립트코어_문법기초/01장_변수/lesson07_변수값_확인하기/01_complete/04.js
@@ -0,0 +1,6 @@
+ /*
+                예제04: name 변수에 저장된 값을 console.log()를 이용해서 웹킷 브라우저의 인스펙터 창의 콘솔 영역에 출력해 주세요.
+             */
+            var name = "ddandongne";
+
+            console.log("name=" + name);
